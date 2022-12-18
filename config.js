b2380c8e68ebd01545a43c04b36541d0ee47f0d2
FrankFontcha <frank.fontcha@vaimanagement.co>
2022-12-18 21:18:03 +0400

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+const app = {
+    dbuser: "frank14",
+    dbpassword: "~`Dotrack_-_~`2022_-_~`Dotrack~",
+    jwtToken: "tP~ggrm&Ro+kZ!!.nV-^x&t1|G|BzFlP|09Rg|%:=+5.HuOT*aIdk|*OCj9l."
+}
+
+exports.app = app;
